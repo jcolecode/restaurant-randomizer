b2490c896b778b2ec201747a33ba525c8dddcb07
jcolecode <jacobcole152@gmail.com>
2024-08-01 16:16:10 -0500

    updates for backend

---
+++ frontend/src/components/Restaurant.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Restaurant = ({ restaurantName }) => {
+  if (!restaurantName) {
+    return <div>No restaurant found.</div>;
+  }
+
+  return (
+    <div className="card mt-4">
+      <div className="card-body">
+        <h5 className="card-title">{restaurantName}</h5>
+      </div>
+    </div>
+  );
+};
+
+export default Restaurant;
